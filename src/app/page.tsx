a3cd16de2c0b1b5e584681678ffd492778437c4e
Roy Lopez <roylopezdev@gmail.com>
2024-01-25 16:42:20 -0300

    Update page layout and add menu buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeMenu from "@/components/Home/Menu";
 
 export default function Home() {
   return (
-    <main className="w-max">
+    <main className="w-full">
       <HomeMenu />
     </main>
   );
--- src/components/Home/Menu/index.tsx
+++ src/components/Home/Menu/index.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 
+const HomeMenuButtons = [
+  {
+    title: "obras terminadas",
+    link: "/obras",
+  },
+  {
+    title: "Comprar Ceramica",
+    link: "/ceramica",
+  },
+  {
+    title: "Encarga tu obra",
+    link: "/encargos",
+  },
+  {
+    title: "Productos Disponibles",
+    link: "/productos",
+  },
+  {
+    title: "Contacto",
+    link: "/contacto",
+  },
+];
+
 const HomeMenu = () => {
-  return <div>HomeMenu</div>;
+  return (
+    <div className="w-full flex flex-row justify-evenly">
+      {HomeMenuButtons.map((button, index) => {
+        return (
+          <button key={index} className="p-6 uppercase">
+            {button.title}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default HomeMenu;
