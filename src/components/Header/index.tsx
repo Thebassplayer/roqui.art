adb5fc885b8888ca66c5c42c7bb8404e0bbcb010
Roy Lopez <roylopezdev@gmail.com>
2024-01-25 16:01:52 -0300

    Fix package name and update header styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full h-1/4 bg-lightBlue">
-      <h1></h1>
+    <div className="w-full h-1/4 bg-lightBlue flex justify-center">
+      <h1 className="p-6">
+        <span className="font-pop text-4xl">ROQI</span>
+        <span className="font-pop text-4xl text-light">.art</span>
+      </h1>
     </div>
   );
 };
