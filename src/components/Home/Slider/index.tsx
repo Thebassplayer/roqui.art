2e6ae4787359cdc69001f6b8c78c9319078c6ee7
Roy Lopez <roylopezdev@gmail.com>
2024-01-27 01:05:36 -0300

    Fix casing of publicId prop in Slider component

--- src/components/Home/Slider/index.tsx
+++ src/components/Home/Slider/index.tsx
@@ -57,7 +57,7 @@ const Slider: React.FC<SliderProps> = ({ velocity = 2000 }) => {
         <h2 className="max-w-md text-4xl">ART</h2>
       </div>
       <div className="flex w-full flex-none flex-col items-center justify-center space-y-4">
-        <SliderImage PublicId={SliderImages[0]} />
+        <SliderImage publicId={SliderImages[0]} />
       </div>
     </div>
   );
