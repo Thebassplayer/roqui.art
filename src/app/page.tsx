ea0fe7cce09c63456a8dc4e138a707a0ccc30c19
Roy Lopez <roylopezdev@gmail.com>
2024-01-27 01:01:58 -0300

    Add Caroussel component and remove Slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
+import Caroussel from "@/components/Caroussel";
 import HomeMenu from "@/components/Home/Menu";
 import ObrasDestacadas from "@/components/Home/ObrasDestacadas";
-import Slider from "@/components/Home/Slider";
 
 export default function Home() {
   return (
     <main className="w-full">
       <HomeMenu />
-      <section className="h-[75vh] w-full">
-        <Slider />
-      </section>
+      <Caroussel />
       <ObrasDestacadas />
     </main>
   );
---
+++ src/components/Caroussel/index.tsx
@@ -0,0 +1,11 @@
+import Slider from "../Home/Slider";
+
+const Caroussel = () => {
+  return (
+    <section className="flex h-[75vh] w-full items-center">
+      <Slider />
+    </section>
+  );
+};
+
+export default Caroussel;
